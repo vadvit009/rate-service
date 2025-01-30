33206d222843e845edd9a535104a519d2c769528
Vadym Vitenko <v.vitenko@4irelabs.com>
2025-01-30 15:01:14 +0200

    fix github comments v1

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+config();
+
+const configService = new ConfigService();
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: configService.get<string>('DATABASE_URL'),
+  schema: 'paypilot',
+  synchronize: false,
+  entities: ['**/*.entity.ts'],
+  migrations: ['src/database/migrations/*-migration.ts'],
+  migrationsRun: false,
+  logging: true,
+});
+
+export default AppDataSource;
